67e1240245f8eb3cd1b006caf19053ff0b2d3657
Martijn Wijnands <martijn.wijnands@gmail.com>
2026-08-13 15:25:29 +0200

    added cloudflare adapter

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,19 +2,20 @@ import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+const artists = defineCollection({
+	// Load Markdown and MDX files in the `src/content/artists/` directory.
+	loader: glob({ base: './src/content/artists', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: ({ image }) =>
 		z.object({
-			title: z.string(),
+			name: z.string(),
 			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: z.optional(image()),
+      category: z.string(),
+      members: z.string().optional(),
+			aliases: z.string().optional(),
+      heroImage: z.optional(image()),
+			homepage: z.string().optional(),
 		}),
 });
 
-export const collections = { blog };
+export const collections = { artists };
